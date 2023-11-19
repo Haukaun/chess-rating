210e47c99b9c70c0dc265d0b47f69be05552a727
Haukaun <haakonfs@hotmail.com>
2023-11-19 21:22:13 +0100

    add: table

--- src/pages/GameHistory.tsx
+++ src/pages/GameHistory.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import RatingTable from "../components/rating-table"
 
 function GameHistory() {
     return (
-        <div>GameHistory</div>
+        <main className="flex flex-col items-center justify-start pt-10 w-full pl-[15rem]">
+
+            <RatingTable />
+            <h1 className="text-3xl min-h-screen pt-10">Game History</h1>
+
+        </main>
     )
 }
 
